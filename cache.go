1ada3697a1ce05a8fcb2b1314395370807caf57a
aabramovdev <aabramov.dev@gmail.com>
2022-05-28 12:55:14 +0500

    My solution for homework 5.2

--- cache.go
+++ cache.go
@@ -2,22 +2,48 @@ package cache
 
 import "time"
 
+const (
+	hoursInHundredYears = 876600
+)
+
+type Value struct {
+	value          string
+	expirationTime time.Time
+}
+
 type Cache struct {
+	cache map[string]Value
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		cache: make(map[string]Value),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	value, ok := c.cache[key]
+	if time.Now().After(value.expirationTime) {
+		delete(c.cache, key)
+		return "", false
+	}
+	return value.value, ok
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.cache[key] = Value{value: value, expirationTime: time.Now().Add(time.Hour * hoursInHundredYears)}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	var keys []string
+	for key, value := range c.cache {
+		if time.Now().Before(value.expirationTime) {
+			keys = append(keys, key)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadLine time.Time) {
+	c.cache[key] = Value{value: value, expirationTime: deadLine}
 }
